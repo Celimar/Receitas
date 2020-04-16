63b877c4e7204a718d56ccc0e717c554703eba8a
Celimar de Oliveira <celimar@gmail.com>
2020-04-16 15:45:28 -0300

    fix erro imagens null de receitas sem imagens, criado dto para edição de imagens

---
+++ src/DevWebReceitas.Application/Mappers/Receitas/ReceitaEditDtoMapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DevWebReceitas.Application.Dtos.Receita;
+using DevWebReceitas.Application.Mappers.Default;
+using DevWebReceitas.Domain.Entities;
+
+namespace DevWebReceitas.Application.Mappers.Receitas
+{
+    public class ReceitaEditDtoMapper : IReceitaEditDtoMapper
+    {
+        public ReceitaEditDto Map(Receita source)
+        {
+            return TypeConverter.ConvertTo<ReceitaEditDto>(source);
+        }
+
+        public IEnumerable<ReceitaEditDto> Map(IEnumerable<Receita> source)
+        {
+            return source.Select(x => Map(x));
+        }
+    }
+}
